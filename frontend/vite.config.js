ab23bd4c838c8cb99c79a2aa3651bb598eba5326
Lochana Jayasinghe <lochanajayasinghe2002@gmail.com>
2026-05-05 14:49:27 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Meka '/' widiyata vitharak thiyanna
   plugins: [react()],
-  base: '/ppw-portfolio-mern/',
 })
